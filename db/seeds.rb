4337f045a74947611e50f0a189cac0fbc344f1c2
dean shmuel <dshmuel110@gmail.com>
2019-06-28 03:10:49 +0300

    seeds file now populating tags,onversations and messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,39 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# require 'json'
-# file_path = './task_1.json'
-
-# raw = File.read(file_path)
-# mails_json = JSON.parse(raw)
-# mails = []
-# mails_json.each do |mail|
-#   convo ={
-#     price_of_reservation: mail["price_of_reservation"].to_f,
-#     numbers_of_guests: mail["numbers_of_guests"].to_i,
-#     days_until_check_out: mail["days_until_check_out"].to_i,
-#     days_until_check_in: mail["days_until_check_in"].to_i,
-#     marker: mail["conversation_id"]
-#     }
-#   mails << convo
-# end
-
-# mails.each do |mail|
-#   conversations = Conversation.where(marker: mail[:marker])
-#   if conversations.empty?
-#     Conversation.create!(mail)
-#   end
-# end
 require 'json'
+require 'csv'
 require 'sentimental'
+
 file_path = './task_1.json'
+
+raw = File.read(file_path)
+mails_json = JSON.parse(raw)
+mails = []
+mails_json.each do |mail|
+  convo ={
+    price_of_reservation: mail["price_of_reservation"].to_f,
+    numbers_of_guests: mail["numbers_of_guests"].to_i,
+    days_until_check_out: mail["days_until_check_out"].to_i,
+    days_until_check_in: mail["days_until_check_in"].to_i,
+    marker: mail["conversation_id"]
+    }
+  mails << convo
+end
+
+mails.each do |mail|
+  conversations = Conversation.where(marker: mail[:marker])
+  if conversations.empty?
+    Conversation.create!(mail)
+  end
+end
+
+#file_path = './task_1.json'
 
 analyzer = Sentimental.new
 analyzer.load_defaults
 analyzer.threshold = 0.2
 
-raw = File.read(file_path)
-mails_json = JSON.parse(raw)
+#raw = File.read(file_path)
+#mails_json = JSON.parse(raw)
 
 mails_json.each do |mail|
   conversation = Conversation.where(marker: mail["conversation_id"])
@@ -49,4 +51,9 @@ mails_json.each do |mail|
     p @message
     @message.save!
   end
+end
+
+
+CSV.foreach('tags.csv') do |row|
+  Tag.create!(tag:row[0])
 end
